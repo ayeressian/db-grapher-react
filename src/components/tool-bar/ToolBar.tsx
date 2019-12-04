f24bc0d2484c27d57e3e652aa1f56151602e24b8
Ara Yeressian <ayeressian2@gmail.com>
2019-12-04 23:10:35 +0400

    init

---
+++ src/components/tool-bar/ToolBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ToolBar.css';
+
+const ToolBar: React.FC = () => {
+  const createTable = () => {
+
+  };
+
+  const createRelation = () => {
+
+  };
+
+  return (
+    <ul className="left_toolbar">
+      <li className="action create_table" title="Create Table" onClick={createTable}></li>
+      <li className="action create_relation" title="Create Relation" onClick={createRelation}></li>
+    </ul>
+  );
+};
+
+export default ToolBar;
